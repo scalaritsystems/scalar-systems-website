31c3fa1d8e271031f8c5b42cbdb61c11fdf7fa73
scalaritsystems <scalaritsystems@gmail.com>
2026-05-31 02:06:44 +0100

    Add footer component

---
+++ components/footer.tsx
@@ -0,0 +1,1 @@
+export default function Footer(){return <footer className='border-t mt-20'><div className='container-custom py-10'><p>© Scalar Systems</p><p>Shrewsbury, Shropshire, United Kingdom</p></div></footer>}
